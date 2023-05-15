f67002a1446cf8bfd4ebde56e75cead61c13feee
suzuyu0115 <szkyut0115@gmail.com>
2023-05-15 18:54:06 +0900

    Add: バブルコンテナを作成

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -73,4 +73,13 @@ class LineBotController < ApplicationController
       contents: bubbles
     }
   end
+
+  def set_bubble(hotel)
+    {
+      type: 'bubble',
+      hero: set_hero(hotel),
+      body: set_body(hotel),
+      footer: set_footer(hotel)
+    }
+  end
 end
