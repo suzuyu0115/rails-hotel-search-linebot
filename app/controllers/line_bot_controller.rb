50a573293a746e7ee58e17a911eb0eca8284567e
suzuyu0115 <szkyut0115@gmail.com>
2023-05-15 22:52:41 +0900

    Add: ヒーローブロックを作成

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -82,4 +82,18 @@ class LineBotController < ApplicationController
       footer: set_footer(hotel)
     }
   end
+
+  def set_hero(hotel)
+    {
+      type: 'image',
+      url: hotel['hotelImageUrl'],
+      size: 'full',
+      aspectRatio: '20:13',
+      aspectMode: 'cover',
+      action: {
+        type: 'uri',
+        uri: hotel['hotelInformationUrl']
+      }
+    }
+  end
 end
